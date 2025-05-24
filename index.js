cf279b2d623462b536f23d5f9e7a5aa8357813ff
maceyhall7834 <maceyhall7834@outlook.com>
2025-05-24 14:02:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
+});
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`);
+  
+  // Set the bot's presence to "Playing Minecraft"
+  bot.editStatus("online", {
+    name: "Minecraft",
+    type: 0 // 0 means "Playing"
+  });
 });
 
 bot.connect(); // Get the bot to connect to Discord
